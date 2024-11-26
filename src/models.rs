33341c521b24328c5ed21f3f14306db1ef9442b3
Rodrigo V Honorato <r.vargashonorato@uu.nl>
2024-11-26 15:45:24 +0100

    add status

--- src/models.rs
+++ src/models.rs
@@ -4,6 +4,7 @@ use axum::{body::Bytes, BoxError};
 use futures::Stream;
 use serde::{Deserialize, Serialize};
 use sqlx::SqlitePool;
+use std::fmt;
 use std::fs;
 use std::path::PathBuf;
 use uuid::Uuid;
@@ -21,8 +22,8 @@ pub struct UploadPayload {
 pub struct Job {
     id: i32,
     pub user_id: i32,
-    status: String, // placeholder
-    loc: PathBuf,   // Path in the filesystem of where this data is saved
+    status: Status,
+    loc: PathBuf,
 }
 
 pub async fn create_jobs_table(pool: &SqlitePool) -> Result<(), sqlx::Error> {
@@ -32,6 +33,7 @@ pub async fn create_jobs_table(pool: &SqlitePool) -> Result<(), sqlx::Error> {
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             user_id INTEGER NOT NULL,
             status TEXT NOT NULL,
+            loc TEXT NOT NULL,
             created_at DATETIME DEFAULT CURRENT_TIMESTAMP
         )
     "#,
@@ -51,7 +53,7 @@ impl Job {
         Job {
             id: 0,
             user_id: 0,
-            status: "".to_string(),
+            status: Status::Pending,
             loc,
         }
     }
@@ -72,9 +74,10 @@ impl Job {
     }
 
     pub async fn add_to_db(&mut self, pool: &SqlitePool) -> Result<(), sqlx::Error> {
-        let result = sqlx::query("INSERT INTO jobs (user_id, status) VALUES (?, ?)")
+        let result = sqlx::query("INSERT INTO jobs (user_id, loc, status) VALUES (?, ?, ?)")
             .bind(self.user_id)
-            .bind(&self.status)
+            .bind(self.loc.to_str())
+            .bind(self.status.to_string())
             .execute(pool)
             .await?;
 
@@ -84,8 +87,47 @@ impl Job {
         Ok(())
     }
 
+    pub async fn update_status(
+        &mut self,
+        status: Status,
+        pool: &SqlitePool,
+    ) -> Result<(), sqlx::Error> {
+        let _result = sqlx::query("UPDATE jobs SET status = ? WHERE id = ?")
+            .bind(status.to_string())
+            .bind(self.id)
+            .execute(pool)
+            .await?;
+
+        self.status = status;
+
+        Ok(())
+    }
+
     pub fn set_user_id(&mut self, user_id: i32) {
         self.user_id = user_id;
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub enum Status {
+    Pending,
+    Processing,
+    Completed,
+    Failed,
+    Queued,
+    Unknown,
+}
+
+impl fmt::Display for Status {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Status::Pending => write!(f, "pending"),
+            Status::Processing => write!(f, "processing"),
+            Status::Completed => write!(f, "completed"),
+            Status::Failed => write!(f, "failed"),
+            Status::Queued => write!(f, "queued"),
+            Status::Unknown => write!(f, "unknown"),
+        }
     }
 }
 
