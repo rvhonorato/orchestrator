fac214507a4d1b2681a211f9ff33d2176b098066
Rodrigo V. Honorato <r.vargashonorato@uu.nl>
2025-10-20 13:53:03 +0200

    add `init_payload_db`

--- src/datasource/db.rs
+++ src/datasource/db.rs
@@ -1,4 +1,5 @@
 use crate::models::job_dto::create_jobs_table;
+use crate::models::payload_dto::create_payload_table;
 use sqlx::{Pool, Sqlite, SqlitePool};
 use tracing::info;
 
@@ -12,6 +13,20 @@ pub async fn init_db(db_path: &str) -> Pool<Sqlite> {
     create_jobs_table(&pool)
         .await
         .expect("failed to create the jobs table");
+
+    pool
+}
+
+pub async fn init_payload_db() -> Pool<Sqlite> {
+    let connection_string = "sqlite::memory:".to_string();
+    info!("Using in-memory database");
+    let pool = SqlitePool::connect(&connection_string)
+        .await
+        .unwrap_or_else(|e| panic!("Database connection failed: {e}"));
+
+    create_payload_table(&pool)
+        .await
+        .expect("failed to create the payloads table");
 
     pool
 }
