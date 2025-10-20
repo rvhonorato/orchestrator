1c2d9867900c881c0c4318a096fe65152e4c2da3
Rodrigo V. Honorato <r.vargashonorato@uu.nl>
2025-10-20 13:53:46 +0200

    add subcommands

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,9 @@ mod models;
 mod routes;
 mod services;
 mod utils;
-use crate::datasource::db::init_db;
 use crate::datasource::fs::init_fs;
 use crate::routes::router::create_routes;
+use crate::{datasource::db::init_db, routes::router::create_client_routes};
 use clap::{Parser, Subcommand};
 use config::loader::Config;
 use services::tasks::{cleaner, getter, sender};
@@ -32,21 +32,6 @@ enum Commands {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let cli = Cli::parse();
-
-    match &cli.command {
-        Commands::Server {} => {
-            start_server().await?;
-        }
-        Commands::Client {} => {
-            start_client().await?;
-        }
-    }
-
-    Ok(())
-}
-
-async fn start_server() -> anyhow::Result<()> {
     // Initialize a logger
     tracing_subscriber::fmt()
         .with_target(false)
@@ -57,6 +42,22 @@ async fn start_server() -> anyhow::Result<()> {
     // Load the configuration
     let config = Config::new().unwrap();
 
+    // Parse command line arguments
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::Server {} => {
+            start_server(config).await?;
+        }
+        Commands::Client {} => {
+            start_client(config).await?;
+        }
+    }
+
+    Ok(())
+}
+
+async fn start_server(config: Config) -> anyhow::Result<()> {
     // Initialize the database
     let pool = init_db(&config.db_path).await;
 
@@ -101,6 +102,22 @@ async fn start_server() -> anyhow::Result<()> {
     Ok(())
 }
 
-async fn start_client() -> anyhow::Result<()> {
-    todo!("Client functionality to be implemented");
+async fn start_client(config: Config) -> anyhow::Result<()> {
+    // Initialize in-memory database
+    let pool = datasource::db::init_payload_db().await;
+
+    // TODO: Add runner task
+
+    // Create app
+    let client_app = create_client_routes(pool.clone(), config.clone());
+
+    // Initialize socket
+    let addr = SocketAddr::from(([0, 0, 0, 0], 9000));
+    tracing::info!("Client listening on {}", addr);
+
+    let listener = TcpListener::bind(addr).await?;
+
+    axum::serve(listener, client_app.into_make_service()).await?;
+
+    Ok(())
 }
