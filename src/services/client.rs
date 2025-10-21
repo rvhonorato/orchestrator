639cc979bfa3819f65c0ee2092ab05407af26f96
Rodrigo V. Honorato <r.vargashonorato@uu.nl>
2025-10-21 12:35:03 +0200

    add test to client service

--- src/services/client.rs
+++ src/services/client.rs
@@ -37,3 +37,51 @@ pub fn execute_payload(payload: &Payload) -> Result<(), ClientError> {
 
     Ok(())
 }
+
+#[cfg(test)]
+mod test {
+
+    use super::*;
+
+    #[test]
+    fn test_execute_payload() {
+        // Prepare a temporary payload
+        let temp_dir = tempfile::tempdir().unwrap();
+        let mut payload = Payload::new();
+        payload.set_loc(temp_dir.path().to_path_buf());
+
+        // Add a simple run.sh script
+        std::fs::write(payload.loc.join("run.sh"), b"#!/bin/bash").unwrap();
+
+        let result = execute_payload(&payload);
+
+        assert!(result.is_ok());
+    }
+
+    #[test]
+    fn test_execute_payload_no_script() {
+        // Prepare a temporary payload
+        let temp_dir = tempfile::tempdir().unwrap();
+        let mut payload = Payload::new();
+        payload.set_loc(temp_dir.path().to_path_buf());
+
+        let result = execute_payload(&payload);
+
+        assert!(matches!(result, Err(ClientError::NoExecScript)));
+    }
+
+    #[test]
+    fn test_execute_payload_script_error() {
+        // Prepare a temporary payload
+        let temp_dir = tempfile::tempdir().unwrap();
+        let mut payload = Payload::new();
+        payload.set_loc(temp_dir.path().to_path_buf());
+
+        // Add a run.sh script that fails
+        std::fs::write(payload.loc.join("run.sh"), b"#!/bin/bash\nexit 1").unwrap();
+
+        let result = execute_payload(&payload);
+
+        assert!(matches!(result, Err(ClientError::Script)));
+    }
+}
